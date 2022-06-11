5fd34581e49c9771b26527ebee24df3fc6d362aa
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2022-06-11 14:04:03 +0300

    Ex. 1.12 - 1.14 are done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,130 +1,68 @@
 import {useState} from "react";
 
-const Button = (props) => {
-    return (
-        <button onClick={props.handler}>
-            {props.text}
-        </button>
-    )
-}
-
-const Statistics = (props) => {
-    return (
-        <div>
-            <table>
-                <StatisticLine text={"Good"}
-                               value={props.goodValue}/>
-
-                <StatisticLine text={"Neutral"}
-                               value={props.neutralValue}/>
-
-                <StatisticLine text={"Bad"}
-                               value={props.badValue}/>
-
-                <StatisticLine text={"All"}
-                               value={props.allValue}/>
-
-                <StatisticLine text={"Score"}
-                               value={props.averageValue}/>
-
-                <StatisticLine text={"Positive"}
-                               value={props.positiveValue}/>
-            </table>
-        </div>
-    )
-}
-
-const StatisticLine = (props) => {
-    if (props.text === "Positive") {
-        return (
-            <tr>
-                <td>{props.text}</td>
-                <td>{props.value} %</td>
-            </tr>
-        )
-    }
-    return (
-        <tr>
-            <td>{props.text}</td>
-            <td>{props.value}</td>
-        </tr>
-    )
-}
-
 const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-    const [all, setAll] = useState(0)
-    const [score, setScore] = useState(0.0)
-    const [positive, setPositive] = useState(0.0)
+    const anecdotes = [
+        'If it hurts, do it more often',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+    ]
 
-    const handleGoodFeedback = () => {
-        const newAll = all + 1
-        const newGood = good + 1
-        setAll(all + 1)
-        setGood(good + 1)
-        setScore((newGood - bad) / newAll)
-        setPositive((newGood / newAll) * 100)
+    const points = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0
     }
 
-    const handleNeutralFeedback = () => {
-        const newAll = all + 1
-        const newNeutral = neutral + 1
-        setAll(newAll)
-        setNeutral(newNeutral)
-        setScore(score / newAll)
-        setPositive((good / newAll) * 100)
+    const [selected, setSelected] = useState(0)
+    const [point, setPoint] = useState(points)
+    const [popular, setPopular] = useState(0)
+
+    const computeTheMostPopular = () => {
+        let mostPopular = 0
+
+        for (let i = 0; i <= 6; i++) {
+            mostPopular = point[i] > point[mostPopular] ? i : mostPopular
+        }
+
+        return mostPopular
     }
 
-    const handleBadFeedback = () => {
-        const newAll = all + 1
-        const newBad = bad + 1
-        setAll(newAll)
-        setBad(newBad)
-        setScore((good - newBad) / newAll)
-        setPositive((good / newAll) * 100)
+    const nextAnecdote = () => {
+        setSelected(Math.floor(Math.random() * anecdotes.length))
     }
 
-    if (all !== 0) {
-        return (
-            <div>
-                <b>Give feedback</b>
-                <br/>
-                <Button handler={handleGoodFeedback}
-                        text={"Good"}/>
-                <Button handler={handleNeutralFeedback}
-                        text={"Neutral"}/>
-                <Button handler={handleBadFeedback}
-                        text={"Bad"}/>
-                <br/>
-                <b>Statistics</b>
-                <Statistics goodValue={good}
-                            badValue={bad}
-                            neutralValue={neutral}
-                            allValue={all}
-                            averageValue={score}
-                            positiveValue={positive}/>
-            </div>
-        )
+    const voteForAnecdote = () => {
+        const copy = {...point}
+        copy[selected] = copy[selected] + 1
+        setPoint(copy)
+        setPopular(computeTheMostPopular)
     }
 
     return (
         <div>
-            <b>Give feedback</b>
+            <h1>Anecdote od the day</h1>
+            {anecdotes[selected]}
             <br/>
-            <Button handler={handleGoodFeedback}
-                    text={"Good"}/>
-            <Button handler={handleNeutralFeedback}
-                    text={"Neutral"}/>
-            <Button handler={handleBadFeedback}
-                    text={"Bad"}/>
+            <p>Has {point[selected]} votes</p>
+            <button onClick={voteForAnecdote}>
+                Vote
+            </button>
+            <button onClick={nextAnecdote}>
+                Next anecdote
+            </button>
+            <h1>The most popular anecdote</h1>
+                {anecdotes[popular]}
             <br/>
-            <b>Statistics</b>
-            <p>No feedback given</p>
         </div>
     )
-
 }
 
 export default App;

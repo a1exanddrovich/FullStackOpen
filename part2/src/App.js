41479cb3e4247aa6cb9a799ecb9689f21ff017d2
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2022-06-12 12:09:36 +0300

    Ex 2.11 is done

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,64 +1,88 @@
-import {useState, useEffect} from "react"
-import axios from "axios"
-import Note from "./components/Note"
+import {useState} from "react";
+
+const Filter = ({handler}) => {
+    return (
+        <div>
+            <h2>The filter</h2>
+            <form>
+                Names to be shown: <input type="text" onChange={handler}/>
+            </form>
+        </div>
+    )
+}
+
+const Persons = ({persons}) => {
+    return (
+        <div>
+            {
+                persons.map(person => <div key={person.id}>{person.name} - {person.phone}</div>)
+            }
+        </div>
+    )
+}
+
+const InfoAdder = (props) => {
+    return (
+        <form onSubmit={props.addNewPersonHandler}>
+            <div>
+                Name: <input type="text" onChange={props.personNameChangeHandler}/>
+                <br/>
+                Phone: <input type="text" onChange={props.phoneChangeHandler}/>
+            </div>
+            <div>
+                <button type="submit">Add</button>
+            </div>
+        </form>
+    )
+}
 
 const App = () => {
-    const [notes, setNotes] = useState([])
-    const [newNote, setNewNote] = useState("")
-    const [showAll, setShowAll] = useState(true)
+    const [persons, setPersons] = useState([])
+    const [showAll, setShowAll] = useState(persons)
+    const [newName, setNewName] = useState("")
+    const [newPhone, setNewPhone] = useState("")
 
-    const hook = () => {
-        axios
-            .get("http://localhost:3001/notes")
-            .then(response => {
-                setNotes(response.data)
-            })
-    }
-
-    useEffect(hook, [])
-
-    const addNote = (event) => {
+    const addNewPerson = (event) => {
         event.preventDefault()
-        const noteObject = {
-            content: newNote,
-            date: new Date().toISOString(),
-            important: Math.random() > 0.5,
-            id: notes.length + 1,
+        const newPersonObject = {
+            id: persons.length + 1,
+            name: newName,
+            phone: newPhone
         }
-
-        setNotes(notes.concat(noteObject))
-        setNewNote("")
+        if (persons.filter(person => person.name === newPersonObject.name).length === 0) {
+            setPersons(persons.concat(newPersonObject))
+            setNewName("")
+            setNewPhone("")
+        } else {
+            alert(`${newPersonObject.name} is already exists in the list`)
+        }
     }
 
-    const handleNoteChange = (event) => {
-        console.log(event.target.value)
-        setNewNote(event.target.value)
+    const handlePersonNameChange = (event) => {
+        setNewName(event.target.value)
     }
 
-    const notesToShow = showAll
-        ? notes
-        : notes.filter(note => note.important)
+    const handlePhoneChange = (event) => {
+        setNewPhone(event.target.value)
+    }
+
+    const filterPersons = (event) => {
+        if (event.target.value !== "") {
+            setShowAll(persons.filter(person => person.name.includes(event.target.value)))
+        } else {
+            setShowAll(persons)
+        }
+    }
 
     return (
         <div>
-            <h1>Notes</h1>
-            <div>
-                <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll ? "important" : "all"}
-                </button>
-            </div>
-            <ul>
-                {notesToShow.map(note =>
-                    <Note key={note.id} note={note}/>
-                )}
-            </ul>
-            <form onSubmit={addNote}>
-                <input
-                    value={newNote}
-                    onChange={handleNoteChange}
-                />
-                <button type="submit">save</button>
-            </form>
+            <Filter handler={filterPersons}/>
+            <h2>The phonebook</h2>
+            <InfoAdder addNewPersonHandler={addNewPerson}
+                       personNameChangeHandler={handlePersonNameChange}
+                       phoneChangeHandler={handlePhoneChange}/>
+            <h2>Numbers</h2>
+            <Persons persons={showAll}/>
         </div>
     )
 }

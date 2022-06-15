f5cc8ca2b647512676702be9de4765fc02e53a39
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2022-06-15 07:03:20 +0300

    Filtering added and states

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,14 +1,47 @@
+import { useState } from "react"
 import Note from "./components/Note"
 
-const App = ({notes}) => {
+const App = (props) => {
+
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState("A new note")
+    const [showAll, setShowAll] = useState(true)
+
+    const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+    const addNote = event => {
+        event.preventDefault()
+        const newNoteObject = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5
+        }
+        setNotes(notes.concat(newNoteObject))
+        setNewNote("")
+    }
+
+    const handleNoteChange = event => {
+        setNewNote(event.target.value)
+    }
+
     return (
         <div>
             <h1>Notes</h1>
+            <button onClick={() => setShowAll(!showAll)}>
+                {
+                    showAll ? "Important only" : "All"
+                }
+            </button>
             <ul>
                 {
-                    notes.map(note => <Note key={note.id} note={note}/>)
+                    notesToShow.map(note => <Note key={note.id} note={note}/>)
                 }
             </ul>
+            <form onSubmit={addNote}>
+                <input type="text" value={newNote} onChange={handleNoteChange}/>
+                <button type="submit">Save</button>
+            </form>
         </div>
     )
 }
